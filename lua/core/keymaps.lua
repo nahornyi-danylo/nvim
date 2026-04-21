9543eb99e2d6a2c0d7b60ead19501e15e7ac7771
Dmytrii Korolov <vacseniorban@gmail.com>
2026-04-21 21:28:58 +0200

    fix haskell errors

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -23,3 +23,23 @@ vim.keymap.set('n', 'zz', function() vim.cmd("cd %:p:h") end)
 vim.keymap.set('n', '<Esc>', function() vim.cmd("nohlsearch") end, { silent = true })
 vim.keymap.set('n', '<leader>t', function() vim.cmd("Themery") end, { silent = true })
 
+local transparent = false
+
+local function ToggleBg()
+  transparent = not transparent
+
+  if transparent then
+    vim.api.nvim_set_hl(0, "Normal",      { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+  else
+    -- grab whatever background your current theme set
+    local hi = vim.api.nvim_get_hl_by_name("Normal", true)
+    if hi and hi.background then
+      local hex = string.format("#%06x", hi.background)
+      vim.api.nvim_set_hl(0, "Normal",      { bg = hex })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = hex })
+    end
+  end
+end
+
+vim.keymap.set("n", "<leader>y", ToggleBg, { desc = "Toggle bg transparency" })
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/dima/.cache/nvim/packer_hererocks/2.1.1744318430/share/lua/5.1/?.lua;/Users/dima/.cache/nvim/packer_hererocks/2.1.1744318430/share/lua/5.1/?/init.lua;/Users/dima/.cache/nvim/packer_hererocks/2.1.1744318430/lib/luarocks/rocks-5.1/?.lua;/Users/dima/.cache/nvim/packer_hererocks/2.1.1744318430/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/dima/.cache/nvim/packer_hererocks/2.1.1744318430/lib/lua/5.1/?.so"
+local package_path_str = "/Users/dima/.cache/nvim/packer_hererocks/2.1.1774896198/share/lua/5.1/?.lua;/Users/dima/.cache/nvim/packer_hererocks/2.1.1774896198/share/lua/5.1/?/init.lua;/Users/dima/.cache/nvim/packer_hererocks/2.1.1774896198/lib/luarocks/rocks-5.1/?.lua;/Users/dima/.cache/nvim/packer_hererocks/2.1.1774896198/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/dima/.cache/nvim/packer_hererocks/2.1.1774896198/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -138,6 +138,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/dima/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["nightfall.nvim"] = {
+    loaded = true,
+    path = "/Users/dima/.local/share/nvim/site/pack/packer/start/nightfall.nvim",
+    url = "https://github.com/2giosangmitom/nightfall.nvim"
   },
   nvim = {
     loaded = true,
